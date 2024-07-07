a3425505ea4af91db4902bda47a190241a65803d
olhaHrb <150930419+olhaHrb@users.noreply.github.com>
2024-07-07 21:41:10 +0300

    form useId

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,5 @@
 import css from "./ContactForm.module.css";
+import { useId } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { nanoid } from "nanoid";
 import * as yup from "yup";
@@ -22,6 +23,8 @@ const ContactFormSchema = yup.object().shape({
 });
 
 const ContactForm = ({ onAdd }) => {
+  const nameFieldId = useId();
+  const numberFieldId = useId();
   const handleSubmit = (values, actions) => {
     onAdd({
       id: nanoid(),
@@ -38,10 +41,17 @@ const ContactForm = ({ onAdd }) => {
       validationSchema={ContactFormSchema}
     >
       <Form className={css.form}>
-        <Field className={css.input} type="text" name="name" />
-        <ErrorMessage name="name" component="span" />
-        <Field className={css.input} type="text" name="number" />
-        <ErrorMessage name="number" component="span" />
+        <label htmlFor={nameFieldId}>Name</label>
+        <Field className={css.input} type="text" name="name" id={nameFieldId} />
+        <ErrorMessage className={css.error} name="name" component="span" />
+        <label htmlFor={numberFieldId}>Number</label>
+        <Field
+          className={css.input}
+          type="text"
+          name="number"
+          id={numberFieldId}
+        />
+        <ErrorMessage className={css.error} name="number" component="span" />
         <button className={css.button} type="Submit">
           Add contact
         </button>
